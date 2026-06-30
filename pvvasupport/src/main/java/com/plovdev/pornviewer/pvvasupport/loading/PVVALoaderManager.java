4b1d62e8c2e8fa5b4d9728454034700c66df5029
plovdev <app67253100@gmail.com>
2026-06-30 23:13:34 +0300

    refactored pvva loder manager

--- pvvasupport/src/main/java/com/plovdev/pornviewer/pvvasupport/loading/PVVALoaderManager.java
+++ pvvasupport/src/main/java/com/plovdev/pornviewer/pvvasupport/loading/PVVALoaderManager.java
@@ -4,19 +4,31 @@ import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.plovdev.pornviewer.core.models.adapter.AdapterInfo;
 import com.plovdev.pornviewer.database.tables.PVVAProvider;
+import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
 import org.plovdev.pvva.models.PVVAHost;
 
 import java.nio.file.Path;
+import java.util.concurrent.CompletableFuture;
 
 public final class PVVALoaderManager {
+    private static final String MAX_SIZE_KEY = "pornviewer.memory.cache-control.max-adapters-loaded.size";
     private static final Cache<String, PVVAHost> CACHED_ADAPTERS = Caffeine.newBuilder()
-            .maximumSize(10)
+            .maximumSize(Integer.getInteger(MAX_SIZE_KEY, 10))
             .build();
     private static final PluginLoader PLUGIN_LOADER = new PluginLoaderImpl();
 
     private PVVALoaderManager() {
         throw new UnsupportedOperationException();
+    }
+
+    public static @NonNull CompletableFuture<PVVAHost> downloadPlugin(String pluginId) {
+        return CompletableFuture.supplyAsync(() -> {
+            PluginDownloader downloader = new PluginDownloader();
+            PVVAHost downloadedHost = downloader.downloadPlugin(pluginId);
+            CACHED_ADAPTERS.put(pluginId, downloadedHost);
+            return downloadedHost;
+        });
     }
 
     public static @Nullable PVVAHost loadPvvaById(String pluginId) {
@@ -27,13 +39,15 @@ public final class PVVALoaderManager {
         if (isDirect) {
             return direclyLoadHost(pluginId);
         } else {
-            return CACHED_ADAPTERS.get(pluginId, PVVALoaderManager::direclyLoadHost);
+            return CACHED_ADAPTERS.getIfPresent(pluginId);
         }
     }
 
     private static PVVAHost direclyLoadHost(String pluginId) {
         AdapterInfo info = PVVAProvider.getAdapterById(pluginId);
-        return PLUGIN_LOADER.loadFromDisk(Path.of(info.pathName()));
+        PVVAHost host = PLUGIN_LOADER.loadFromDisk(Path.of(info.pathName()));
+        CACHED_ADAPTERS.put(pluginId, host);
+        return host;
     }
 
     public static PVVAHost forceAdapter(String pluginId) {
@@ -41,6 +55,10 @@ public final class PVVALoaderManager {
         PVVAHost freshHost = direclyLoadHost(pluginId);
         CACHED_ADAPTERS.put(pluginId, freshHost);
         return freshHost;
+    }
+
+    public static boolean isCached(String pluginId) {
+        return CACHED_ADAPTERS.getIfPresent(pluginId) != null;
     }
 
     public static void clearCache() {
---
+++ pvvasupport/src/main/java/com/plovdev/pornviewer/pvvasupport/loading/PluginDownloader.java
@@ -0,0 +1,9 @@
+package com.plovdev.pornviewer.pvvasupport.loading;
+
+import org.plovdev.pvva.models.PVVAHost;
+
+public class PluginDownloader {
+    public PVVAHost downloadPlugin(String systemPluginId) {
+
+    }
+}
